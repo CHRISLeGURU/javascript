4e41a1815dd8e6da4e9eb130c5f937cb903a0cce
CHRISLeGURU <163886791+CHRISLeGURU@users.noreply.github.com>
2024-07-20 11:54:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+
+alert("Hello World !");
+//var nom="Balume Dieumerci";
+//var age=28;
+//alert(nom+' a '+age+' ans ');
+//var nom=prompt("Entrer votre Nom:");
+//var nom=prompt("Entrer votre age:");
+//alert(nom+' a '+age+' ans ');
+
+//var n1=parseInt(prompt("Entrer le premier valeur"));
+//var n2=parseInt(prompt("Entrer le deuxieme valeur"));
+//var somme = n1+n2;
+//alert(n1+" + "+n2+" = "+somme);
+
+//var n1=parseInt(prompt("Entrer la valeur"));
+//var square = n1*n1;
+//alert(square +n1+"est"+resultat);
+
+//Exercice 6 Boucle
+
+//var nom=prompt("Entrer votre nom");
+//for(var i=0; i<10; i++){
+   // alert(nom);
+   // }
+    //while(i<10){
+      //  alert(nom);
+       // i++;
+   // }
+
+//EXERCICES FONCTION
+//function add2nbre(a,b){
+   // return a+b;
+//}
+
+
+//function div2nbre(x,y){
+   // if(y>0){
+       // return x/y
+    //}else{
+       // alert("Erreur !!");
+   // }
+//} 
+
+//var n1=parseInt(prompt("Entrer N1"));
+//var n1=parseInt(prompt("Entrer N2"));
+//alert(n1+" / "+n2+" = "+div2nbre(n1,n2) );
+
+/*function exe1(){
+    var age=document.getElementById("age").value;
+    if(age>=18){
+        document.getElementById("resultat").value="Majeur";
+    }else{
+        document.getElementById("resultat").value="Mineur"
+
+    }
+}*/
+
+
